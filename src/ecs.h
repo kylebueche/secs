83e525e39096576db3a4b9cb3290976f0d116f99
Kyle Bueche <kyle.bueche@gmail.com>
2026-06-30 16:55:08 -0700

    Simplifying Sparse Set wrapping over std::vector

--- src/ecs.h
+++ src/ecs.h
@@ -1,21 +1,18 @@
-// Copyright (c) 2026 Kyle Bueche.
-// Author: Kyle Bueche.
-// This project is licensed under the MIT Licence - see LICENSE.txt.
-// No warranty implied.
+// Copyright (c) 2026 Kyle Bueche
+// SPDX-License-Identifier: MIT
+// Author: Kyle Bueche
 
 #ifndef SECS_ECS_H_
 #define SECS_ECS_H_
 
-#include <stdint.h>
+#include <cstdint>
 #include <vector>
 #include <type_traits>
 #include <memory>
 
-using EntityID = uint32_t;
+using EntityID = std::size_t;
 
 // A simple Sparse Set.
-// This structure does not resize on insert().
-// Resizing must be done explicitely.
 template <typename T>
 struct SparseSet
 {
@@ -26,44 +23,32 @@ struct SparseSet
     std::vector<SparseID> dense;
     std::vector<T> values;
     std::size_t numValues;
-    std::size_t capacity;
 
     SparseSet() = delete;
 
     explicit SparseSet(std::size_t capacity) :
-        capacity(capacity),
         sparse(std::vector<DenseID>(capacity)),
         dense(std::vector<SparseID>(capacity)),
-        values(std::vector<T>(capacity)),
+        values(std::vector<T>()),
         numValues(0) {
-        // Start mapping bijective
+        // Initialize bijective mapping
         for (int i = 0; i < capacity; i++) {
             sparse[i] = i;
             dense[i] = i;
         }
+        values.reserve(capacity);
     }
 
-    void assure_capacity(std::size_t requiredSize) {
-        if (requiredSize <= capacity) return;
-        // Grow exponentially
-        std::size_t newCapacity = std::max(requiredSize, std::size_t(capacity * 1.5));
-        sparse.resize(newCapacity);
-        dense.resize(newCapacity);
-        values.resize(newCapacity);
-        // Maintain bijective mapping
-        for (std::size_t i = capacity; i < newCapacity; i++) {
-            sparse[i] = i;
-            dense[i] = i;
-        }
-        capacity = newCapacity;
+    std::size_t sparseSize() const {
+        return sparse.size();
+    }
+    std::size_t sparseCapacity() const {
+        return sparse.capacity();
     }
 
     // Vector-like functions
     void clear() {
-        numValues = 0;
-    }
-    std::size_t size() {
-        return numValues;
+        values.clear();
     }
     T& operator[](std::size_t index) {
         return values[sparse[index]];
@@ -71,44 +56,50 @@ struct SparseSet
     const T& operator[](std::size_t index) const {
         return values[sparse[index]];
     }
+    std::size_t size() const {
+        return values.size();
+    }
+    std::size_t capacity() const {
+        return values.capacity();
+    }
     T* data() {
         return values.data();
     }
-    void resize(std::size_t newCapacity) {
-        sparse.resize(newCapacity);
-        dense.resize(newCapacity);
-        values.resize(newCapacity);
-        capacity = newCapacity;
-        if (numValues > capacity) {
-            numValues = newCapacity;
+    void reserve(std::size_t requiredSize) {
+        if (requiredSize <= capacity()) return;
+        std::size_t oldSparseSize = sparseSize();
+        sparse.resize(requiredSize);
+        dense.resize(requiredSize);
+        values.reserve(requiredSize);
+        // Maintain bijective mapping
+        for (std::size_t i = oldSparseSize; i < sparseSize(); i++) {
+            sparse[i] = i;
+            dense[i] = i;
         }
     }
-
-    [[nodiscard]] bool isRegistered(SparseID sparseID) const {
-        return sparseID < capacity && sparse[sparseID] < numValues;
+    void resize(std::size_t newCapacity) {
+        reserve(newCapacity);
+        values.resize(newCapacity);
     }
-    [[nodiscard]] bool isFull() const {
-        return numValues >= capacity;
+
+    [[nodiscard]] bool isValidSparseID(SparseID sparseID) const {
+        return sparseID < sparseSize() && sparse[sparseID] < size();
     }
 
     // Main tooling
-    SparseID insert(T value) {
-        assure_capacity(numValues + 1);
-
-        SparseID sparseID = dense[numValues];
-        values[numValues] = std::move(value);
-        numValues++;
-
-        return sparseID;
+    SparseID push_back(T value) {
+        reserve(size() + 1);
+        SparseID sparseID = dense[size()];
+        values.push_back(value);
     }
 
     bool insertAt(SparseID sparseID, T value) {
-        if (isRegistered(sparseID)) return false;
-        assure_capacity(sparseID + 1);
+        if (isValidSparseID(sparseID)) return false;
+        reserve(sparseID + 1);
 
         DenseID currentDensePos = sparse[sparseID];
+        swapDense(numValues, currentDensePos);
         std::swap(dense[numValues], dense[currentDensePos]);
-        sparse[dense[currentDensePos]] = currentDensePos;
         sparse[dense[numValues]] = numValues;
         values[numValues] = std::move(value);
         numValues++;
@@ -116,18 +107,20 @@ struct SparseSet
     }
 
     void remove(SparseID sparseID) {
-        if (!isRegistered(sparseID)) return;
+        if (!isValidSparseID(sparseID)) return;
 
-        DenseID denseID = sparse[sparseID]; // Store the dense index
-        DenseID tailDenseID = numValues - 1; // Find the tail entry of the dense list.
-        SparseID tailSparseID = dense[tailDenseID]; // Find its sparse ID
+        DenseID denseID = sparse[sparseID];
+        DenseID tailDenseID = numValues - 1;
 
-        std::swap(dense[denseID], dense[tailDenseID]); // Swap
-        std::swap(values[denseID], values[tailDenseID]); // ..
-        numValues--; // & pop!
+        swapDense(denseID, tailDenseID); // Swap...
+        values.pop_back(); // & pop!
+    }
 
-        sparse[tailSparseID] = denseID; // Point the old tail's sparse ID to its new dense index
-        sparse[sparseID] = tailDenseID;
+private:
+    void swapDense(DenseID i, DenseID j) {
+        std::swap(sparse[dense[i]], sparse[dense[j]]);
+        std::swap(dense[i], dense[j]);
+        std::swap(values[i], values[j]);
     }
 };
 
@@ -138,17 +131,58 @@ template <class... ComponentTypes>
 class EcsRegistry
 {
 public:
-    std::tuple<Component<ComponentTypes>...> componentList;
+    struct ECS_BUILTIN_EntityAlive {};
+
+    std::tuple<Component<ECS_BUILTIN_EntityAlive>, Component<ComponentTypes>...> componentList;
 
     template <typename T>
     auto registerComponent() {
-        return EcsRegistry<ComponentTypes..., std::decay_t<T>>(std::move(std::tuple_cat(std::move(componentList), std::tuple<Component<T>>())));
+        static_assert(!(std::is_same_v<ComponentTypes, T> || ...));
+        return EcsRegistry<ComponentTypes..., std::decay_t<T>>(
+            std::move(std::tuple_cat(std::move(componentList), std::tuple<Component<T>>())));
     }
 
     template <typename T>
     Component<T>& getComponent() {
         return std::get<T>(componentList);
     }
+
+    EntityID newEntity() {
+        getComponent<ECS_BUILTIN_EntityAlive>().insert({});
+    }
+
+    void killEntity(EntityID entity) {
+        getComponent<ECS_BUILTIN_EntityAlive>().remove(entity);
+        (getComponent<ComponentTypes>().remove(entity), ...);
+    }
+
+    // Aligns an archetype of components in memory so that
+    // the dense data is contiguous in an SoA format.
+    // Returns the size of the archetype.
+    template <typename... Components>
+    std::size_t align() {
+        std::size_t minSize = min(getComponent<Components>().size() ...);
+        std::size_t archetypeSize = 0;
+
+        auto iterateSmallestList = [&](auto& component) {
+            if (component.size() == minSize) {
+                for (std::size_t i = 0; i < minSize; i++) {
+                    EntityID entityID = component.dense[i];
+                    if ((getComponent<Components>().isRegistered(entityID) && ...)) {
+                        (getComponent<Components>().swapDense(i, archetypeSize), ...);
+                        archetypeSize++;
+                    }
+                }
+                return true;
+            }
+            return false;
+        };
+
+        (iterateSmallestList(getComponent<Components>()) || ...);
+        return archetypeSize;
+    }
+
+
 };
 
 #endif //SECS_ECS_H_
